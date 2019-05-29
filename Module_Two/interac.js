f8b7e2ed929bb512fb2501421ab3857c3c6737a7
Geovanni <gonsalo_gm@hotmail.com>
2019-05-28 22:17:09 -0500

    Se implemento interaccion con la ventana mnodal de los coins

--- Module_Two/interac.js
+++ Module_Two/interac.js
@@ -1,5 +1,6 @@
 (function () {
     'use strict';
+    var modal, ul_coins, u;
     //INICIO del Modulo de Navegacion Movil
     function nav_scroll() {
         var scrolly, nv, div_im, ul, modal;
@@ -51,10 +52,52 @@
         });
         //-----------Para cerrar el div y el menu-------------
     }
+    //----------INICIO DEL MODAL-----------
+    function show(e) {
+        e.stopPropagation();
+        var t_span, h2, img, text;
+        t_span = e.target.className;
+        h2 = document.querySelector(".coin > h2");
+        img = document.querySelector(".qr > img");
+        text = document.querySelector("input");
+
+        function asignar(propina, icon, value) {
+            modal.setAttribute("class", "modal show");
+            h2.innerHTML = propina;
+            img.setAttribute("src", icon);
+            text.setAttribute("value", value);
+        }
+
+        if (t_span === "icon-bitcoin") {
+            asignar("APORTE BITCOIN", "images/binance_bitcoin.png", "1PZh2W88B1wSAYyB5ZwyY5zexKnjM2DC8S");
+        } else if (t_span === "icon-ethereum") {
+            asignar("APORTE ETHERIUM", "images/uphold_ethereum.png", "0x09bDb8e103162220CA91EE01D1cF87098E2FCac1");
+        } else if (t_span === "icon-monero") {
+            asignar("APORTE MONERO", "images/poloniex_monero.png", "4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS2YWqhhgA9S13zRqHH");
+        }
+    }
+    function closeModal(e) {
+        if (e.target.className === "modal show") {
+            modal.setAttribute("class", "modal");
+        }
+    }
+    //-------INICIO DEL MODAL----------
+    //FIN del Modulo de Navegacion Movil
+    
     function execute() {
         //INICIO del modulo NAV MOVIL
         document.querySelector(".nav_movil > .ul_movil").addEventListener("click", toogle_movil);
         window.addEventListener("scroll", nav_scroll);
+        //INICIO DEL MODAL
+        modal = document.querySelector("div.modal");
+        
+        ul_coins = document.querySelectorAll("ul.coins");
+        for (u = 0; u < ul_coins.length; u += 1) {
+            ul_coins[u].addEventListener("click", show);
+        }
+        document.querySelector("div.box_coins").addEventListener("click", closeModal);
+        //FIN DEL MODAL
+        //FIN del modulo NAV MOVIL
     }
     window.addEventListener("load", execute);
 }());
