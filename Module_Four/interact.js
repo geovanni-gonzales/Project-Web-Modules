33509f398daf2008d436ebc325ad420e066c5fb3
Geovanni <gonsalo_gm@hotmail.com>
2019-06-06 22:52:34 -0500

    Codificacion JS para la interaccion de aside mediante una animacion Toggle y Static en el scroll

--- Module_Four/interact.js
+++ Module_Four/interact.js
@@ -1,9 +1,32 @@
 (function(){
     'use strict';
-    var Variables;
+    function fixed() {
+        //var posY = window.scrollY; alias de pageYOffset
+        var posY, boton, heightHead;
+        posY = window.pageYOffset;
+        boton = document.getElementsByClassName("boton")[0];
+        heightHead = document.getElementsByTagName("header")[0].offsetHeight;
+        
+        if (posY > heightHead) {
+            boton.style.position = "fixed";
+            boton.style.top = "0px";
+        } else {
+            boton.style.position = "static";
+        }
+    }
+    
+    function toggle() {
+        document.getElementsByTagName("aside")[0].classList.toggle("toggle");
+    }
+    function close() {
+        document.getElementsByTagName("aside")[0].classList.remove("toggle");
+        
+    }
     
     function execute() {
-        //se ejecutan al cargar el documeto.
+        document.getElementsByClassName("boton")[0].addEventListener("click", toggle);
+        window.addEventListener("scroll", fixed);
+        document.getElementsByClassName("articleContainer")[0].addEventListener("click", close);
     }
     
     window.addEventListener("load", execute);
